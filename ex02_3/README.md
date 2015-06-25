7961260b8e70b7dcda9aeb4f0a0d55c35fc93462
Jannis Limperg <jannis@limperg.de>
2015-06-25 02:24:44 +0200

    ex02_3: Internalises friendly-time package
    friendly-time is currently not on Stackage, and since it's just a single
    file that's unlikely to change much, I just copied it.

---
+++ ex02_3/README.md
@@ -0,0 +1,3 @@
+This project incorporates code from the
+[`friendly-time`](http://hackage.haskell.org/package/friendly-time) Hackage
+package by Pat Brisbin.
--- ex02_3/shell.nix
+++ ex02_3/shell.nix
@@ -1,15 +1,16 @@
 with (import <nixpkgs> {}).pkgs;
 let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, containers, friendly-time, lens-family
-             , lens-family-th, pretty, stdenv, time
+            ({ mkDerivation, base, containers, lens-family, lens-family-th
+             , old-locale, pretty, stdenv, time
              }:
              mkDerivation {
                pname = "newsfeed";
                version = "0.1.0.0";
                src = ./.;
+               isLibrary = true;
+               isExecutable = true;
                buildDepends = [
-                 base containers friendly-time lens-family lens-family-th pretty
-                 time
+                 base containers lens-family lens-family-th old-locale pretty time
                ];
                homepage = "https://github.com/JLimperg/java2015";
                description = "Haskell interpretation of ex02_3";
